2de9ff404c7f1ab59c4200f5fa0b14df6a80cb68
Oleksandr Podolich <45513720+AlexPodolich@users.noreply.github.com>
2026-07-28 14:53:11 +0200

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        
+        int l = 0;
+
+        int res = 0;
+
+        for (int r = 1; r < prices.length; r++){
+            while(prices[l] > prices[r]){
+                l++;
+            }
+            res = Math.max(res, prices[r] - prices[l]);
+        }
+
+        return res;
+    }
+}
